1eee67aaee1955907b0e0a83c5c0c2a31a829ea7
Peter Keating <pete.keating@etchuk.com>
2022-12-12 15:36:23 +0000

    Created module using module boilerplate template

---
+++ Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Author = "Etch UK Ltd.",
+    Category = "Performance",
+    Description = "Provides Output caching for Orchard Core.",
+    Name = "Output Caching",
+    Version = "0.0.1",
+    Website = "https://etchuk.com"
+)]
